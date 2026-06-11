7ff7c4fc4e6f8a9693bc1c5c01c0f115b418a245
Haseeb Chaudhury <98563266+Haseebzz@users.noreply.github.com>
2026-06-11 17:32:25 -0400

    Update Rising-temp.sql

--- Rising-temp.sql
+++ Rising-temp.sql
@@ -1,4 +1,5 @@
-Write a solution to find all dates' `id` with higher temperatures compared to its previous dates (yesterday).
+--Write a solution to find all dates' `id` with higher temperatures compared to its previous dates (yesterday). 
+--use lag window function to make a new row which gets previous row values of temperature and recordarte so we can compare and use where condition datediff.
 SELECT id
 FROM (
     SELECT
@@ -11,3 +12,12 @@ FROM (
 ) x
 WHERE temperature > prev_temp
   AND DATEDIFF(recordDate, prev_date) = 1;
+
+--we can alternatively use a self join so for each row will map to all ids like a cartesian product and can perform the where clausee 
+ select w1.id 
+ from Weather w1 
+ join Weather w2 
+where datediff(w1.recordDate,w2.recordDate) = 1 
+and w1.temperature > w2.temperature 
+
+--
