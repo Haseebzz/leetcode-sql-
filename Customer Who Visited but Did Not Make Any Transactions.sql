3c62ecce01d5ab86213c985b5411f7f260665b6a
Haseeb Chaudhury <98563266+Haseebzz@users.noreply.github.com>
2026-06-10 20:38:29 -0400

    Enhance SQL query for customers without transactions
    Added an alternative query using left join to find customers who visited but did not make any transactions.

---
+++ Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,19 @@
+
+--one way to solve this is to see if the visit id is not in the transactions table using where clause 
+SELECT
+    customer_id,
+    COUNT(*) AS count_no_trans
+FROM Visits
+WHERE visit_id NOT IN (
+    SELECT visit_id
+    FROM Transactions
+)
+GROUP BY customer_id; 
+
+--another way is to use a left join which gets all of the customer and visit id customer and find where trans_id is null means customer didnt do a transaction 
+select customer_id, count(*) as count_no_trans
+from Visits 
+left join Transactions 
+on Visits.visit_id = Transactions.visit_id 
+where Transactions.transaction_id is null 
+group by customer_id 
