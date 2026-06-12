73e3542351e0b61e2a883234f05b611e22c34c4a
Haseeb Chaudhury <98563266+Haseebzz@users.noreply.github.com>
2026-06-11 21:29:37 -0400

    Implement SQL query for monthly transaction statistics
    Added SQL query to calculate monthly transactions by country.

---
+++ Monthly Transactions I.sql
@@ -0,0 +1,18 @@
+--Write an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
+
+SELECT
+    DATE_FORMAT(trans_date,'%Y-%m') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    SUM(case when state='approved' then 1 else 0 END) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+   SUM(
+    CASE
+        WHEN state = 'approved' THEN amount
+        ELSE 0
+    END
+) as approved_total_amount
+FROM Transactions
+GROUP BY
+    DATE_FORMAT(trans_date,'%Y-%m'),
+    country;
